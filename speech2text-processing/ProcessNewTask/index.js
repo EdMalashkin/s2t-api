6283dd9c64ec2992e980e32003b5da37be8de8b0
Edward Malashkin <emalashkin@gmail.com>
2023-02-20 19:38:01 +0100

    first working version

--- speech2text-processing/ProcessNewTask/index.js
+++ speech2text-processing/ProcessNewTask/index.js
@@ -1,58 +1,26 @@
 const axios = require('axios');
 
 module.exports = async function (context, documents) {
-    if (!!documents && documents.length > 0) {
-        context.log('Document Id: ', documents[0].id);
-    }
-
-    // var promise1 = new Promise(function (resolve, reject) {
-    //     setTimeout(function () {
-    //         resolve('foo');
-    //         context.log('setTimeout: ', documents[0].id);
-    //     }, 3000);
-
-    // });
-    // context.log(promise1);
-    // let resolve = await promise1;
-    // context.log(resolve);
-    // context.log('resolve: ', documents[0].id);
-
     documents.forEach(async(doc) => {
-        context.log('Document: ', doc);
-        const transcript = await GetTranscript2();
+        console.log('Document: ', doc);
+        const transcript = await GetTranscript();
         doc.data = transcript;
-        context.log('Document with transcript', doc);
+        console.log('Document with transcript', doc);
         await SaveTranscript(doc);
-        
-        // try {
-        //     const { data } = await axios.get('https://youtubetranscript.azurewebsites.net/api/youtube-transcript', 
-        //                         { params: { id: 'CbJjrDjVe2U', lang: 'uk' } });
-        //     context.log('data:', data);   
-        // } catch (err) {
-        //     context.log('err:', err);
-        // }
     });
 }
 
-async function GetTranscript2(){
+async function GetTranscript(){
     try {
         const { data } = await axios.get('https://youtubetranscript.azurewebsites.net/api/youtube-transcript', 
                             { params: { id: 'CbJjrDjVe2U', lang: 'uk' } })
+        console.log('GetTranscript', data);                            
         return data;
       } catch (err) {
-        //console.log(err);
+        console.log(err);
       }
 }
-//GetTranscript2();
 
-function GetTranscript(){
-    axios.get('https://youtubetranscript.azurewebsites.net/api/youtube-transcript', 
-                    { params: { id: 'CbJjrDjVe2U', lang: 'uk' } })
-            .then(resp => {
-                context.log('resp: ', resp);
-                //console.log(resp);
-            });
-}
 
 async function SaveTranscript(doc){
     try {
@@ -60,7 +28,7 @@ async function SaveTranscript(doc){
         console.log(url);
         const config = { 'content-type': 'application/json' };
         const response = await axios.post(url, doc, config);
-        console.log(response.data);
+        console.log('SaveTranscript', response.data);
     } catch (err) {
         console.error(err);
     }
@@ -74,4 +42,4 @@ async function Test() {
     };
     await SaveTranscript(doc);
 }
-Test();
+//Test();
