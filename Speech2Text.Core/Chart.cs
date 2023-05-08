46c2a11f8021b6897981b31cf406b4bfbed88525
Edward Malashkin <emalashkin@gmail.com>
2023-05-08 08:10:21 +0200

    add chart

---
+++ Speech2Text.Core/Chart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flurl.Http;
+
+namespace Speech2Text.Core
+{
+    public class Chart
+    {
+        private readonly string text;
+
+        public Chart(string text)
+        {
+            this.text = text;
+        }
+
+        public async Task<Stream> GetPng()
+        {
+            var chart = await "https://quickchart.io/wordcloud"
+            .PostJsonAsync(new { format = "png", width = "300", height = "300", fontScale = "15", scale = "linear", text = this.text })
+            .ReceiveStream();
+            return chart;  
+        }
+    }
+}
