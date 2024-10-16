a546f6ea12e2ebab47ebf2494672e911eee6ceb5
Edward Malashkin <emalashkin@gmail.com>
2024-10-16 14:39:51 +0200

    duplicate ProcessedAt and Error to task

--- Speech2Text.Core/Models/Transcript.cs
+++ Speech2Text.Core/Models/Transcript.cs
@@ -18,7 +18,9 @@ namespace Speech2Text.Core.Models
         public string OriginalURL { get; set; } = string.Empty;
         public string Language { get; set; } = string.Empty;
         public DateTime? CreatedAt { get; set; }
-    }
+		public DateTime? ProcessedAt { get; set; }
+		public string? Error { get; set; }
+	}
 
     public class Transcript : TranscriptTask
 	{
@@ -28,9 +30,5 @@ namespace Speech2Text.Core.Models
 
         [JsonProperty(Order = 10)] // to be the last one
         public JArray? Data { get; set; }
-
-        public DateTime? ProcessedAt { get; set; }
-
-        public string? Error { get; set; }
 	}
 }
