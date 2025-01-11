6ea511926a318b691657e1fbe83bb2decec4ec14
Edward Malashkin <emalashkin@gmail.com>
2025-01-11 15:36:23 +0100

    add theme field

--- Speech2Text.Core/Models/TokenStats.cs
+++ Speech2Text.Core/Models/TokenStats.cs
@@ -16,10 +16,10 @@ namespace Speech2Text.Core.Models
 			if (transcript != null && transcript.Data != null)
 			{
 				var result = transcript.Data
-					.SelectMany(x => x["tokens"].Select(token => (string)token["lemma"]))
-					.Where(x => x.Length > 0)
-					.GroupBy(x => x)
-					.Select(g => new { Word = g.Key.ToUpper(), Freq = g.Count(), Links = GetLinks(g.Key) })
+					.SelectMany(x => x["tokens"]
+						.Select(token => new { Word = (string)token["lemma"], Theme = (string)token["theme"] }))
+					.GroupBy(x => new {x.Word, x.Theme })
+					.Select(g => new { Word = g.Key.Word.ToUpper(), g.Key.Theme, Freq = g.Count(), Links = GetLinks(g.Key.Word) })
 					.Where(x => x.Freq >= minfreq)
 					.OrderByDescending(g => g.Freq)
 					.ThenBy(g => g.Word)
